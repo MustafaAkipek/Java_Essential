ec290c0b61d08b920de924e219ab0ad37b24796c
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-10-22 21:35:42 +0300

    variables

---
+++ variables/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int number = 10;
+        double number2 = 10.5;
+        char character = 'A';
+        String message = "Number of students: ";
+        System.out.println(message + number);
+        System.out.println(number2);
+        System.out.println(character);
+    }
+}
