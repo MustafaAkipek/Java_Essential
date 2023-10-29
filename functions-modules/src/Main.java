7a4da8e42136edf7625d20b69c49454408d32901
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-10-29 11:24:50 +0300

    functions1

--- functions-modules/src/Main.java
+++ functions-modules/src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        numberPuzzle();
+        giveMessage("Gek");
+    }
+
+    public static void numberPuzzle() {
+        int[] numbers = new int[]{1, 2, 5, 7, 9, 0};
+        int tobeSearched = 5;
+        boolean isThere = false;
+
+        for (int number: numbers) {
+            if(number == tobeSearched) {
+                isThere = true;
+                break;
+            }
+        }
+
+        if(isThere) {
+            System.out.println("The number is available: " + tobeSearched);
+        }else{
+            System.out.println("The number is not available: " + tobeSearched);
+        }
+    }
+
+    public static void giveMessage(String message) {
+        System.out.println(message);
     }
 }
