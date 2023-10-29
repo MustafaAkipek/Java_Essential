6980437e77c3d30fef00359a9e06c3e7dd6d36d0
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-10-29 11:32:46 +0300

    function2

---
+++ functions-modules2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = sumfunc(5, 7);
+        System.out.println(sum);
+    }
+
+    public static int sumfunc(int number1, int number2) {
+        return number1 + number2;
+    }
+    
+}
