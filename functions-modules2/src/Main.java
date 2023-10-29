4498ea3714f2e63e61002d77cc0e0c3ee3866c74
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-10-29 11:39:47 +0300

    ARGS

--- functions-modules2/src/Main.java
+++ functions-modules2/src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int sum = sumfunc(5, 7);
-        System.out.println(sum);
+        int sum1 = sumfunc1(5, 7);
+        System.out.println(sum1);
+
+        int sum2 = sumfunc2(5, 7, 8, 9, 1, 4, 2, 6);
+        System.out.println(sum2);
     }
 
-    public static int sumfunc(int number1, int number2) {
+    public static int sumfunc1(int number1, int number2) {
         return number1 + number2;
     }
-    
+
+    public static int sumfunc2(int... numbers) {
+        int sum = 0;
+        for(int number:numbers) {
+            sum += number;
+        }
+        return sum;
+    }
 }
